49d1c471b7043ac7f39c78a090686b1a0d023315
Zombie Brain <nal888@mail.ru>
2021-12-12 15:54:18 +0300

    endoendoendoendoendo

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ const mongoUri =
 const bot = new Telegraf("5044857735:AAF2WxOFiJuzzSTp_k4OEZpZEmD5AwnP9Os");
 
 bot.command("/export", async (ctx) => {
-  // Explicit usage
-
   var get = await Message.find();
 
   var buf = Buffer.from(JSON.stringify(get));
 
   ctx.replyWithDocument({ source: buf, filename: "export.json" });
 });
+
+bot.command("/showHistory", async (ctx) => {
+  var get = await Message.find();
+
+  var toShow = "";
+  get.forEach((item) => {
+    toShow += `time: ${item.date}, message: ${item.text} \n`;
+  });
+  await ctx.reply(toShow);
+  //ctx.replyWithDocument({ source: buf, filename: "export.json" });
+});
+
 bot.on("text", async (context) => {
   //await context.reply(context.update.message.text);
 
@@ -41,37 +51,6 @@ bot.on("text", async (context) => {
   });
 });
 
-// bot.on("photo", async (context) => {
-//   const file = context.update.message.photo;
-//   var fileId = file[file.length - 1].file_id;
-//   context.telegram.getFileLink(fileId).then((url) => {
-//     axios({ url: url.toString(), responseType: "stream" }).then((response) => {
-//       new Promise((resolve, reject) => {
-//         response.data
-//           .pipe(
-//             fs.createWriteStream(`./temp/${context.update.message.from.id}.jpg`)
-//           )
-//           .on("finish", () => console.log("save"))
-//           .on("error", (e) => console.log(e.message));
-//       });
-//     });
-//   });
-//   console.log("here");
-
-//   vk.upload.messagePhoto({
-//     peer_id: 198478609,
-//     source: {
-//       value: fs.createReadStream(
-//         `./temp/${context.update.message.from.id}.jpg`
-//       ),
-//       options: {
-//         filename: `${context.update.message.from.id}.jpg`,
-//         contentType: "image/jpeg",
-//       },
-//     },
-//   });
-// });
-
 const vk = new VK({
   token:
     "e20b41306867488cdfdcfedbd94251e65ddbb810e246f1a68591f143b6854afa5a6dacb51dd3dfefa9223",
@@ -79,15 +58,23 @@ const vk = new VK({
 
 // #region VKAPI
 vk.updates.on("message_new", async (context) => {
-  //await context.send(context.text);
-
   if (context.text === "/export") {
     var get = await Message.find();
 
     var buf = Buffer.from(JSON.stringify(get));
 
     context.sendDocuments({ value: buf, filename: "export.json" });
-    // ctx.replyWithDocument({ source: buf, filename: "export.json" });
+    return;
+  }
+
+  if (context.text === "/showHistory") {
+    var get = await Message.find();
+
+    var toShow = "";
+    get.forEach((item) => {
+      toShow += `time: ${item.date}, message: ${item.text} \n`;
+    });
+    context.send(toShow);
     return;
   }
 
